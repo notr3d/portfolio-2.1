833d17cb20314f31c915f9cd62fd3d4cb39ac6ce
notr3d <lxmrrr@gmail.com>
2017-04-05 19:14:34 +0300

    19:14

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-var toggleButton = document.querySelector('.js-button');
+/*var toggleButton = document.querySelector('.js-button');
 toggleButton.addEventListener('click', function(){
 	this.classList.toggle('active');
 });
+
 
 var body = document.querySelector('body');
 var siteAsideToggleButton = document.querySelector('.site-aside__toggle-button');
@@ -11,21 +12,50 @@ siteAsideToggleButton.addEventListener('click', function(){
 });
 
 var modal = document.querySelector('.modal');
-var modalButton = document.querySelector('#modal');
-modalButton.addEventListener('click', function(){
-	openModal();
-});
+var modalButton = document.querySelectorAll('.modal-open');
+for (var i = 0; i < modalButton.length; i++){
+	modalButton[i].addEventListener('click', function(){
+		var target = this.dataset['target'];
+		openModal(target);
+	});
+};
 var modalCloseButton = document.querySelector('.modal__close');
 modalCloseButton.addEventListener('click', function(){
 	closeModal();
 });
 
 
-var openModal = function(){
+var openModal = function(target){
 	modal.classList.add('active');
-	$(modal).children().load('html/test.html')
+	//$(modal).children().load('html/test.html')
+	loadDoc(target);
 }
 
 var closeModal = function(){
 	modal.classList.remove('active');
 }
+
+function loadDoc(target) {
+	target = 'html/' + target + '.html';
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.querySelector('.modal__wrapper').innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", target, true);
+  xhttp.send();
+}*/
+
+var toggleButtons = document.querySelectorAll('.js-toggle');
+for (var i = 0; i < toggleButtons.length; i++){
+	toggleButtons[i].addEventListener('click', function(){
+		this.classList.toggle('active');
+	});
+};
+
+var siteAsideToggleButton = document.querySelector('.site-aside__toggle-button');
+siteAsideToggleButton.addEventListener('click', function(){
+	document.body.classList.toggle('aside-on');
+});
